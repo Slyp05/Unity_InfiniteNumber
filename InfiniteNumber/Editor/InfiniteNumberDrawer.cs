c16af3ac5ecdbf49f7b94050b5273d04b86d413d
Lucas Sarkadi <63003495+Slyp05@users.noreply.github.com>
2020-11-08 23:55:15 +0100

    updated drawer
    Added tooltip and scrollable (left/right) label

--- InfiniteNumber/Editor/InfiniteNumberDrawer.cs
+++ InfiniteNumber/Editor/InfiniteNumberDrawer.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+using System.Reflection;
+using UnityEditor;
 using UnityEngine;
 
 /*
@@ -15,15 +16,42 @@ using UnityEngine;
 [CustomPropertyDrawer(typeof(InfiniteNumber))]
 public class InfiniteNumberDrawer : PropertyDrawer
 {
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
     {
+        // get serialized property
         SerializedProperty valueStringProp = property.FindPropertyRelative("serializedValue");
-        
-        EditorGUI.BeginProperty(position, label, property);
+
+        // get tooltip
+        string tooltipText = null;
+        TooltipAttribute tooltip = GetTooltip(fieldInfo, true);
+        if (tooltip != null)
+            tooltipText = tooltip.tooltip;
+
+        // cache rect width
+        float rectWidth = rect.width;
+
+        // show label as int field for scroll capabilities
+        rect.width = EditorGUIUtility.labelWidth + 2;
+
+        EditorGUI.BeginChangeCheck();
+            long scrollVal = EditorGUI.LongField(rect, new GUIContent(property.displayName, tooltipText), 0);
+        if (EditorGUI.EndChangeCheck())
+        {
+            InfiniteNumber nb = new InfiniteNumber(valueStringProp.stringValue);
+            nb += scrollVal;
+
+            valueStringProp.stringValue = nb.ToString();
+        }
+
+        rect.x += rect.width;
+        rect.width = rectWidth - rect.width;
+
+        // show text area
+        EditorGUI.BeginProperty(rect, label, property);
         {
             EditorGUI.BeginChangeCheck();
-                string newVal = EditorGUI.DelayedTextField(position, new GUIContent(property.displayName), 
-                    InfiniteNumber.FormatString(valueStringProp.stringValue));
+            string newVal = EditorGUI.DelayedTextField(rect, new GUIContent(""), InfiniteNumber.FormatString(valueStringProp.stringValue),
+                EditorStyles.numberField);
             if (EditorGUI.EndChangeCheck())
                 valueStringProp.stringValue = newVal;
         }
@@ -33,5 +61,12 @@ public class InfiniteNumberDrawer : PropertyDrawer
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
         return EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+    }
+
+    TooltipAttribute GetTooltip(FieldInfo field, bool inherit)
+    {
+        TooltipAttribute[] attributes = field.GetCustomAttributes(typeof(TooltipAttribute), inherit) as TooltipAttribute[];
+
+        return attributes.Length > 0 ? attributes[0] : null;
     }
 }
